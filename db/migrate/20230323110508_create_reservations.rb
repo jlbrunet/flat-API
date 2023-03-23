b020828d0a84c2c4edab1000bf0f3319633ffb56
Julie Brunet <juliebrunet21@msn.com>
2023-03-23 12:12:05 +0100

    models created

---
+++ db/migrate/20230323110508_create_reservations.rb
@@ -0,0 +1,11 @@
+class CreateReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.string :start_date
+      t.string :end_date
+      t.references :listing, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
